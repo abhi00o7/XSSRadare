8ee88c441860b17c0c6202af16785e75a6ed19b7
Abhishek Kumar <31090188+abhi00o7@users.noreply.github.com>
2021-02-09 04:35:13 +0530

    Update banner.py
    changed the name for keeping track of personal changes in the script.

--- banner.py
+++ banner.py
@@ -5,10 +5,7 @@ import time
 
 def banner():
     return """
- __  _____ ___ ___         _              
- \ \/ / __/ __| _ \__ _ __| |__ _ _ _ ___
-  >  <\__ \__ \   / _` / _` / _` | '_/ -_)
- /_/\_\___/___/_|_\__,_\__,_\__,_|_| \___|
+▂▃▄▅▆▇█▓▒░XSS_SCANNER░▒▓█▇▆▅▄▃▂
 
 [+] XSSRadare Start working at : {0}
 """.format(time.ctime())
